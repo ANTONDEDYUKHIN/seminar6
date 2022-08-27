89fd81b056d931eb5216de51f271f04e22ce6062
ANTON <19111984@list.ru>
2022-08-27 19:46:37 +0700

    reverseArr

---
+++ Task39/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Написать программу, которая перевернет одномерный массив
+(послед эл-т будет на 1м месте, а 1й на последнем) 1 2 3 4 5 - 5 4 3 2 1 */
+Console.Clear();
+int[] CreateArrayRndInt(int size, int min, int max)// метод создания массива (размер, мин, макс число интервала)
+{
+int[] array = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < size; i++)
+{
+    array[i] = rnd.Next(min, max+1);
+}
+return array;
+}
+
+void PrintArray(int[] array)// метод вывода массива на консоль
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void ReverseArray(int [] array)
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+    while (index1 < index2)
+    {
+        int obj = array[index1];
+        array[index1] = array[index2];
+        array[index2] = obj;
+        index1 ++;
+        index2 --;
+    }
+}
+
+int[] arr = CreateArrayRndInt(5, 1, 9);
+PrintArray(arr);
+ReverseArray(arr);
+PrintArray(arr);
