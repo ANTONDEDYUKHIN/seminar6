3478bb9969a80ec3c288f7f01bdd72a51d800c7f
ANTON <19111984@list.ru>
2022-08-27 20:52:55 +0700

    10-2.2

---
+++ Task42-2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Написать прогр., которая будет преобр-ть десятичное
+число в двоичное 45 - 101101, 3 - 11, 4 - 10*/
+Console.Clear();
+Console.Write("введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+int result = 0;
+int mult = 1;
+while(number > 0)
+{
+    result += number % 2 * mult;
+    number /= 2;
+    result *= 10;
+}
+Console.WriteLine(result);
