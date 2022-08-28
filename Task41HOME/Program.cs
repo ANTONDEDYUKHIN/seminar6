ba850451c6d13956d31fcf99f2cbac3f5017d22f
ANTON <19111984@list.ru>
2022-08-28 09:23:41 +0700

    quantPosNum

---
+++ Task41HOME/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3 */
+Console.Clear();
+Console.Write("Введите количество чисел М: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] EnterIntNum(int size)
+{
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+   Console.Write($"Введите число M{i + 1}: "); 
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+return array;
+}
+
+int[] array1 = EnterIntNum(size);
+int Count(int[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i] > 0)
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine(Count(array1));
